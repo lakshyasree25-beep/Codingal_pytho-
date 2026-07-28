349a82462c26f9672d7bf64ec617a0e9b1a9bb20
lakshyasree25-beep <lakshyasree25@gmail.com>
2026-07-28 18:52:43 -0500

    hi

--- activity_1.py
+++ activity_1.py
@@ -1,1 +1,10 @@
-'happy birthday'
+#Print a Sentence
+print ("hlowww person")
+#Print a Number
+print (7)
+#Next Line after print
+print ("Water\n")
+#Pass multiple arguments for printing
+print ("I realy",6+7,"need water") 
+#End argument of Print Statement.
+print("I really need water", end=".")
